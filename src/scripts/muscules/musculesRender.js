b63f45f2789422461ed02ca07acfe62dd0cf7198
m1xture <misha.ivanov.1050@gmail.com>
2024-05-25 23:09:28 +0300

    render cards

--- src/scripts/muscules/musculesRender.js
+++ src/scripts/muscules/musculesRender.js
@@ -1,19 +1,53 @@
-async function renderMuscules() {
-  function render({ name, filter, imgUrl }) {
-    document.querySelector(".exercises__list").insertAdjacentHTML(
-      "beforeend",
-      `<li class="exercises__item" style="background-image: url(${imgUrl})">
+function render({ name, filter, imgUrl }) {
+  document.querySelector(".exercises__list").insertAdjacentHTML(
+    "beforeend",
+    `<li class="exercises__item" style="background-image: linear-gradient(rgba(16, 16, 16, 0.7), rgba(16, 16, 16, 0.7) ), url(${imgUrl})">
+     
     <div>
-      <h3 class="exercises__subtitle">${name}</h3>
+      <h3 class="exercises__subtitle">${
+        name.charAt(0).toUpperCase() + name.slice(1)
+      }</h3>
       <p class="exercises__text">${filter}</p>
     </div>
   </li>`
-    );
-  }
+  );
+}
+
+async function renderMuscules() {
   const resp = await fetch(
     "https://energyflow.b.goit.study/api/filters?filter=Muscles&page=1&limit=12"
   );
   const { results } = await resp.json();
   results.forEach(render);
 }
+document.querySelector(".pagination").addEventListener("click", async (e) => {
+  if (e.target.nodeName === "LI" || e.target.nodeName === "P") {
+    let newPage = 1;
+    if (
+      e.target.classList.contains("pagination__item--active") ||
+      e.target.parentNode.classList.contains("pagination__item--active")
+    )
+      return;
+    if (e.target.nodeName === "LI") {
+      Array.from(e.target.parentNode.children).forEach((child) =>
+        child.classList.remove("pagination__item--active")
+      );
+      e.target.classList.add("pagination__item--active");
+
+      newPage = Number(e.target.querySelector("p").textContent);
+    } else {
+      Array.from(e.target.parentNode.parentNode.children).forEach((child) =>
+        child.classList.remove("pagination__item--active")
+      );
+      e.target.parentNode.classList.add("pagination__item--active");
+      newPage = Number(e.target.textContent);
+    }
+    document.querySelector(".exercises__list").innerHTML = "";
+    const resp = await fetch(
+      `https://energyflow.b.goit.study/api/filters?filter=Muscles&page=${newPage}&limit=12`
+    );
+    const { results } = await resp.json();
+    results.forEach(render);
+  }
+});
 renderMuscules();
