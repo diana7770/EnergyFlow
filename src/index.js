bf227b6c75ad6b47c1304429b558ec4382265081
m1xture <misha.ivanov.1050@gmail.com>
2024-05-28 17:46:35 +0300

    burger connect

--- src/index.js
+++ src/index.js
@@ -1,8 +1,3 @@
 import "./scripts/muscules/musculesRender";
 import "./scripts/muscules/renderCarts";
-
-
-
-
-
-
+import "./scripts/burgerMenu";
--- src/scripts/burgerMenu.js
+++ src/scripts/burgerMenu.js
@@ -4,10 +4,12 @@ function toggleBurger() {
     .classList.toggle("burger--hidden");
 }
 
-document
-  .querySelector("[data-burger-open]")
-  .addEventListener("click", toggleBurger);
+document.querySelector("[data-burger-open]").addEventListener("click", () => {
+  toggleBurger();
+  document.body.style.overflow = "hidden";
+});
 
-document
-  .querySelector("[data-burger-close]")
-  .addEventListener("click", toggleBurger);
+document.querySelector("[data-burger-close]").addEventListener("click", () => {
+  toggleBurger();
+  document.body.style.overflow = "visible";
+});
