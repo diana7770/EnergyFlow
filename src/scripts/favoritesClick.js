c92ff1405d66fc3a74d4d14de7515ec9d9421be7
diana7770 <20diane09@gmail.com>
2024-06-01 09:57:27 +0300

    favorites-notCompleted

---
+++ src/scripts/favoritesClick.js
@@ -0,0 +1,5 @@
+// const btnFavorite = document.querySelector(".favorites__btn");
+
+// btnFavorite.addEventListener("click", function () {
+//     document.querySelector("main").innerHTML = '  '
+// });
