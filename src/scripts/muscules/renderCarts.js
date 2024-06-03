598fbd9d031cad6c5fe0f7590d3853cecbb64aae
m1xture <misha.ivanov.1050@gmail.com>
2024-06-03 17:13:17 +0300

    update

--- src/scripts/muscules/renderCarts.js
+++ src/scripts/muscules/renderCarts.js
@@ -1,10 +1,11 @@
+import popupListener from "../popups/popupOpen";
+
 const list = document.querySelector(".exercises__list");
 const title = document.querySelector(".exercises__title");
 const spanFis = document.getElementById("span-ss");
 const boxSserh = document.getElementById("box-searh");
 const renmSpan = document.querySelector(".renm-sp");
 const pagination = document.querySelector(".pagination");
-
 
 // function decreaseNumber() {
 //     console.log('Currently at ' + oneClick);
@@ -20,17 +21,16 @@ list.addEventListener("click", async (event) => {
     list.remove();
     document
       .querySelector(".exercises .container")
-      .insertAdjacentHTML("beforeend", `<ul class=exercises__list data-muscles=${nameCart}></ul>`);
-
+      .insertAdjacentHTML(
+        "beforeend",
+        `<ul class=exercises__list data-muscles=${nameCart}></ul>`
+      );
 
     pagination.remove();
 
-    
-    
     spanFis.classList.remove("unFisitble");
     boxSserh.classList.remove("unFisitble");
     list.innerHTML = " ";
-
 
     // console.log(renmSpan)
     renmSpan.textContent = nameCart;
@@ -39,12 +39,12 @@ list.addEventListener("click", async (event) => {
       `https://energyflow.b.goit.study/api/exercises?muscles=${nameCart.toLowerCase()}&page=1&limit=10`
     );
     const data = await resp.json();
-    console.log(data)
+    console.log(data);
     document
       .querySelector(".exercises .container")
       .insertAdjacentHTML("beforeend", "<ul class=pagination></ul>");
     for (let i = 1; i <= data.totalPages; i++) {
-      console.log(1)
+      console.log(1);
       document.querySelector(".pagination").insertAdjacentHTML(
         "beforeend",
         `<li class="pagination__item">
@@ -52,54 +52,66 @@ list.addEventListener("click", async (event) => {
     </li>`
       );
       if (i === 1) {
-        document.querySelector(".pagination__item").classList.add("pagination__item--active")
+        document
+          .querySelector(".pagination__item")
+          .classList.add("pagination__item--active");
       }
     }
     data.results.forEach(renderCarts);
+    popupListener();
+    document
+      .querySelector(".pagination")
+      .addEventListener("click", async (e) => {
+        console.log(123);
+        if (e.target.nodeName === "LI" || e.target.nodeName === "P") {
+          let newPage = 1;
+          if (
+            e.target.classList.contains("pagination__item--active") ||
+            e.target.parentNode.classList.contains("pagination__item--active")
+          )
+            return;
+          if (e.target.nodeName === "LI") {
+            Array.from(e.target.parentNode.children).forEach((child) =>
+              child.classList.remove("pagination__item--active")
+            );
+            e.target.classList.add("pagination__item--active");
 
-
-    document.querySelector(".pagination").addEventListener("click", async (e) => {
-      console.log(123)
-      if (e.target.nodeName === "LI" || e.target.nodeName === "P") {
-        let newPage = 1;
-        if (
-          e.target.classList.contains("pagination__item--active") ||
-          e.target.parentNode.classList.contains("pagination__item--active")
-        )
-          return;
-        if (e.target.nodeName === "LI") {
-          Array.from(e.target.parentNode.children).forEach((child) =>
-            child.classList.remove("pagination__item--active")
+            newPage = Number(e.target.querySelector("p").textContent);
+          } else {
+            Array.from(e.target.parentNode.parentNode.children).forEach(
+              (child) => child.classList.remove("pagination__item--active")
+            );
+            e.target.parentNode.classList.add("pagination__item--active");
+            newPage = Number(e.target.textContent);
+          }
+          document.querySelector(".exercises__list").innerHTML = "";
+          const resp = await fetch(
+            `https://energyflow.b.goit.study/api/exercises?muscles=${document
+              .querySelector(".exercises__list")
+              .dataset.muscles.toLowerCase()}&page=${newPage}&limit=10`
           );
-          e.target.classList.add("pagination__item--active");
-    
-          newPage = Number(e.target.querySelector("p").textContent);
-        } else {
-          Array.from(e.target.parentNode.parentNode.children).forEach((child) =>
-            child.classList.remove("pagination__item--active")
-          );
-          e.target.parentNode.classList.add("pagination__item--active");
-          newPage = Number(e.target.textContent);
+          const { results } = await resp.json();
+          results.forEach(renderCarts);
         }
-        document.querySelector(".exercises__list").innerHTML = "";
-        const resp = await fetch(
-        `https://energyflow.b.goit.study/api/exercises?muscles=${document.querySelector(".exercises__list").dataset.muscles.toLowerCase()}&page=${newPage}&limit=10`
-        );
-        const { results } = await resp.json();
-        results.forEach(renderCarts)
-      }
-    });
-    
+      });
   }
 });
 
-function renderCarts({ bodyPart, burnedCalories, name, rating, target, time }) {
+function renderCarts({
+  bodyPart,
+  burnedCalories,
+  name,
+  rating,
+  target,
+  time,
+  _id,
+}) {
   const str = name;
   const str2 = target;
   // console.log(str)
   document.querySelector(".exercises__list").insertAdjacentHTML(
     "beforeend",
-    `<li class="exercises__item-body">
+    `<li class="exercises__item-body" data-id=${_id}>
                <div class="top-sec">
                  <h2 class="h-top">WORKOUT</h2>
                  <p class="star-num">${rating.toFixed(1)}</p>
@@ -119,7 +131,10 @@ function renderCarts({ bodyPart, burnedCalories, name, rating, target, time }) {
       <div class="buttom-sec">
                 <p class="text-buttom-all">Burned calories: <p class="text-buttom-no">${burnedCalories} / ${time} min</p></p>
                 <p class="text-buttom-all">Body part: <p class="text-buttom-no">${bodyPart}</p></p>
-      <p class="text-buttom-all">Target: <p class="text-buttom-no">${str2.slice(0, 7)}...</p></p>
+      <p class="text-buttom-all">Target: <p class="text-buttom-no">${str2.slice(
+        0,
+        7
+      )}...</p></p>
                </div>
              </li>`
   );
---
+++ src/scripts/popups/popupOpen.js
@@ -0,0 +1,132 @@
+import Toast from "typescript-toastify";
+
+export default function popupListener() {
+  const refs = {
+    listEl: document.querySelector("[data-muscles]"),
+    popupEl: document.querySelector(".popup-backdrop"),
+    gifEl: document.querySelector("[data-popup-img]"),
+    titleEl: document.querySelector("[data-popup-title]"),
+    ratingTextEl: document.querySelector(".rating__text"),
+    targetEl: document.querySelector("[data-popup-target]"),
+    bodypartEl: document.querySelector("[data-popup-bodypart]"),
+    equipmentEl: document.querySelector("[data-popup-equip]"),
+    popularEl: document.querySelector("[data-popup-popular]"),
+    caloriesEl: document.querySelector("[data-popup-calories]"),
+    descriptionEl: document.querySelector("[data-popup-description]"),
+    pasteValues({
+      _id,
+      bodyPart,
+      equipment,
+      gifUrl,
+      name,
+      target,
+      description,
+      rating,
+      burnedCalories,
+      time,
+      popularity,
+    }) {
+      this.popupEl.setAttribute("data-id", _id);
+      this.titleEl.textContent = name.charAt(0).toUpperCase() + name.slice(1);
+      this.gifEl.style.backgroundImage = `url(${gifUrl})`;
+      this.ratingTextEl.textContent = rating.toFixed(1);
+      this.targetEl.textContent =
+        target.charAt(0).toUpperCase() + target.slice(1);
+      this.bodypartEl.textContent =
+        bodyPart.charAt(0).toUpperCase() + bodyPart.slice(1);
+      this.equipmentEl.textContent =
+        equipment.charAt(0).toUpperCase() + equipment.slice(1);
+      this.popularEl.textContent = popularity;
+      this.caloriesEl.textContent = `${burnedCalories}/${time} min`;
+      this.descriptionEl.textContent = description;
+    },
+  };
+
+  async function getInfo(id) {
+    const resp = await fetch(
+      `https://energyflow.b.goit.study/api/exercises/${id}`
+    );
+    const data = await resp.json();
+    return data;
+  }
+
+  refs.listEl.addEventListener("click", async (e) => {
+    if (e.target === e.currentTarget) return;
+    let id = e.target.dataset.id;
+    if (e.target.nodeName !== "LI") {
+      id = e.target.closest(".exercises__item-body").dataset.id;
+    }
+    const infoObj = await getInfo(id);
+    refs.pasteValues(infoObj);
+    refs.popupEl.classList.remove("popup--hidden");
+  });
+  refs.popupEl.addEventListener("click", async (e) => {
+    if (e.target.dataset.closePopup)
+      e.currentTarget.classList.add("popup--hidden");
+    if (e.target.dataset.addFavorites) {
+      if (JSON.parse(localStorage.getItem("favorites"))) {
+        const isAdded = JSON.parse(localStorage.getItem("favorites")).some(
+          ({ _id }) => _id === refs.popupEl.dataset.id
+        );
+        if (isAdded)
+          return new Toast({
+            position: "top-right",
+            toastMsg: "💪 This exercise has already been added to favorites",
+            autoCloseTime: 3000,
+            canClose: true,
+            // showProgress: true,
+            pauseOnHover: true,
+            pauseOnFocusLoss: true,
+            type: "default",
+            theme: "light",
+          });
+      }
+      const info = await getInfo(refs.popupEl.dataset.id);
+      if (localStorage.getItem("favorites")) {
+        try {
+          const currentFavorites = JSON.parse(
+            localStorage.getItem("favorites")
+          );
+          currentFavorites.push(info);
+          localStorage.setItem("favorites", JSON.stringify(currentFavorites));
+          new Toast({
+            position: "top-right",
+            toastMsg: "🩶 Added to favorites",
+            autoCloseTime: 2500,
+            canClose: true,
+            // showProgress: true,
+            pauseOnHover: true,
+            pauseOnFocusLoss: true,
+            type: "default",
+            theme: "light",
+          });
+        } catch (err) {
+          console.log(err);
+        }
+      } else {
+        localStorage.setItem("favorites", JSON.stringify([info]));
+        new Toast({
+          position: "top-right",
+          toastMsg: "🩶 Added to favorites",
+          autoCloseTime: 2500,
+          canClose: true,
+          // showProgress: true,
+          pauseOnHover: true,
+          pauseOnFocusLoss: true,
+          type: "default",
+          theme: "light",
+        });
+      }
+    }
+    if (e.target.dataset.giveRating) {
+      const id = e.currentTarget.dataset.id;
+      e.currentTarget.classList.add("popup--hidden");
+      document
+        .querySelector(".review-backdrop")
+        .classList.remove("review-backdrop--hidden");
+      document.querySelector(".review__rating").value = Number(
+        refs.ratingTextEl.textContent
+      );
+    }
+  });
+}
